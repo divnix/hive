859b199fea9240ab5620cbb8ba2687090bec3353
David Arnold <david.arnold@iohk.io>
2023-04-10 18:11:58 -0500

    fix: use haumea v0.20.0

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
     disko.url = "github:divnix/blank";
     nixos-generators.url = "github:divnix/blank";
     home-manager.url = "github:divnix/blank";
-    haumea.url = "github:nix-community/haumea?ref=refs/pull/2/head";
+    haumea.url = "github:nix-community/haumea?ref=v0.2.0";
     haumea.inputs.nixpkgs.follows = "nixpkgs";
   };
 
