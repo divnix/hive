669cdfcf61823d33f11a4fe5ee1f3c34903f4eaa
David Arnold <david.arnold@iohk.io>
2023-04-23 12:01:44 -0500

    feat: used haumea's scoped importers and freeze hive.load semantics

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
     disko.url = "github:divnix/blank";
     nixos-generators.url = "github:divnix/blank";
     home-manager.url = "github:divnix/blank";
-    haumea.url = "github:nix-community/haumea?ref=v0.2.0";
+    haumea.url = "github:nix-community/haumea?ref=v0.2.1";
     haumea.inputs.nixpkgs.follows = "nixpkgs";
   };
 
@@ -37,9 +37,14 @@
       with builtins;
         haumea.lib.load (removeAttrs args ["cell" "inputs"]
           // {
+            loader = haumea.lib.loaders.scoped;
+            transformer = with haumea.lib.transformers; [
+                liftDefault
+                (hoistLists "_imports" "imports")
+            ];
             # `self` in paisano refers to `inputs.self.sourceInfo` 😕
             # but is disallowed in haumea
-            inputs = removeAttrs (inputs // {inherit cell;}) ["self"];
+            inputs = removeAttrs (inputs // {inherit inputs cell;}) ["self"];
           });
   in
     haumea.lib
