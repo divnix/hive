ee75bb605178fee0b0e06b3cda97a55b5b0b00d4
vincent.cui <privat@vincentcui.de>
2023-07-28 20:26:25 +0200

    fix modules and profiles collector operations

--- src/collectors/_ops.nix
+++ src/collectors/_ops.nix
@@ -18,7 +18,7 @@ in {
       } @ moduleInputs: let
         evaled = module (renamer cell target) moduleInputs;
       in {
-        imports = l.attrByPath ["imports"] evaled [];
+        imports = l.attrByPath ["imports"] [] evaled;
         options.bee.modules."${renamer cell target}" =
           {
             enable = lib.mkOption {
@@ -27,8 +27,8 @@ in {
               description = "En-/Disable module ${renamer cell target}";
             };
           }
-          // (l.attrByPath ["options"] evaled {});
-        config = lib.mkIf (config.bee.modules."${renamer cell target}".enable) (l.attrByPath ["config"] evaled {});
+          // (l.attrByPath ["options"] {} evaled);
+        config = lib.mkIf (config.bee.modules."${renamer cell target}".enable) (l.attrByPath ["config"] {} evaled);
       })))
     (l.mapAttrs (target: checks.modules))
   ];
@@ -41,13 +41,19 @@ in {
       } @ moduleInputs: let
         evaled = profile (renamer cell) moduleInputs;
       in {
-        imports = l.attrByPath ["imports"] evaled [];
+        imports = l.attrByPath ["imports"] [] evaled;
         options.bee._profiles."${renamer cell target}".enable = lib.mkOption {
           type = lib.types.bool;
           default = lib.elem (renamer cell target) config.bee.profiles;
           description = "En-/Disable profile ${renamer cell target}";
         };
-        config = lib.mkIf (config.bee._profiles."${renamer cell target}".enable) (l.attrByPath ["config"] evaled {});
+        config =
+          lib.mkIf
+          (config.bee._profiles."${renamer cell target}".enable) (
+            if l.hasAttr "config" evaled
+            then evaled.config
+            else evaled
+          );
       })))
     (l.mapAttrs (target: checks.profiles))
   ];
