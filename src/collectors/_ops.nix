46301fc8082fbd7f6c27175feb6a6734731a3fc6
vincent.cui <privat@vincentcui.de>
2023-07-28 15:21:10 +0200

    revert modules collector operations

--- src/collectors/_ops.nix
+++ src/collectors/_ops.nix
@@ -11,6 +11,24 @@
     ;
 in {
   modules = renamer: system: cell: [
+    (l.mapAttrs (target: module: ({
+        config,
+        lib,
+        ...
+      } @ moduleInputs: let
+        evaled = module (renamer cell target) moduleInputs;
+      in {
+        options.bee.modules."${renamer cell target}" =
+          {
+            enable = lib.mkOption {
+              type = lib.types.bool;
+              default = false;
+              description = "En-/Disable module ${renamer cell target}";
+            };
+          }
+          // evaled.options;
+        config = lib.mkIf (config.bee.modules."${renamer cell target}".enable) evaled.config;
+      })))
     (l.mapAttrs (target: checks.modules))
   ];
 
@@ -27,6 +45,6 @@ in {
         };
         config = lib.mkIf (config.bee._profiles."${renamer cell target}".enable) (profile profileInputs);
       })))
-      (l.mapAttrs (target: checks.profiles))
+    (l.mapAttrs (target: checks.profiles))
   ];
 }
