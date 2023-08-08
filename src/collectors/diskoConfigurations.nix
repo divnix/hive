9aa6e9c6b2481c780ae999487d394a3b456730f1
David Arnold <david.arnold@iohk.io>
2023-08-08 19:58:34 +0200

    fix: disko configuration typo; rm unused code

--- src/collectors/diskoConfigurations.nix
+++ src/collectors/diskoConfigurations.nix
@@ -9,7 +9,6 @@
 
   inherit (root) requireInput walkPaisano;
 
-  # same as pasteurize, but for disko where the system doesn't matter
   sing = self:
     walkPaisano self cellBlock (system: cell: [
       (l.filterAttrs (_: _: "x86_64-linux" == system)) # pick one
