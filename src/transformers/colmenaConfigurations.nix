0a8ce17efe6c448feb7d209459dcd7d14a50797f
guangtao <gtrunsec@hardenedlinux.org>
2023-10-19 23:13:17 -0700

    feat: add colmena darwin support

--- src/transformers/colmenaConfigurations.nix
+++ src/transformers/colmenaConfigurations.nix
@@ -7,31 +7,40 @@
   locatedConfig,
 }: let
   l = nixpkgs.lib // builtins;
-
-  inherit (root) beeModule transformers;
+  inherit (root) transformers;
   inherit (inputs) colmena;
 
-  colmenaModules = l.map (l.setDefaultModuleLocation (./colmenaConfigurations.nix + ":colmenaModules")) [
-    # these modules are tied to the below schemaversion
-    # so we fix them here
-    colmena.nixosModules.assertionModule
-    colmena.nixosModules.keyChownModule
-    colmena.nixosModules.keyServiceModule
-    colmena.nixosModules.deploymentOptions
-    {
-      environment.etc."nixos/configuration.nix".text = ''
-        throw '''
-          This machine is not managed by nixos-rebuild, but by colmena.
-        '''
-      '';
-    }
-  ];
+  isDarwin = evaled.config.bee.pkgs.stdenv.isDarwin;
+
+  colmenaModules =
+    l.map (l.setDefaultModuleLocation (./colmenaConfigurations.nix + ":colmenaModules")) [
+      # these modules are tied to the below schemaversion
+      # so we fix them here
+      colmena.nixosModules.assertionModule
+      colmena.nixosModules.keyChownModule
+      colmena.nixosModules.deploymentOptions
+      {
+        environment.etc."nixos/configuration.nix".text = ''
+          throw '''
+            This machine is not managed by nixos-rebuild, but by colmena.
+          '''
+        '';
+      }
+    ]
+    ++ (l.optionals (!isDarwin) [colmena.nixosModules.keyServiceModule]);
 
   config = {
     imports = [locatedConfig] ++ colmenaModules;
   };
 in
-  transformers.nixosConfigurations {
-    inherit evaled;
-    locatedConfig = config;
-  }
+  if isDarwin
+  then
+    transformers.darwinConfigurations {
+      inherit evaled;
+      locatedConfig = config;
+    }
+  else
+    transformers.nixosConfigurations {
+      inherit evaled;
+      locatedConfig = config;
+    }
