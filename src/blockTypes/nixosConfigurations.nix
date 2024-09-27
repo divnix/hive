5bfb051863513479c2717d2ea9e2cf54aa8c981f
sntx <sntx@sntx.space>
2024-09-26 22:29:28 -0600

    feat: added nixos-rebuild actions to blockTypes/nixosConfigurations

--- src/blockTypes/nixosConfigurations.nix
+++ src/blockTypes/nixosConfigurations.nix
@@ -17,7 +17,40 @@
     type = "nixosConfiguration";
     # nixosGenerator's actions?
     # microvm action?
-    # nixos-rebuild action?
+    actions = {
+      currentSystem,
+      fragment,
+      fragmentRelPath,
+      target,
+      inputs,
+    }: let
+      getString = o: (l.elemAt (l.splitString ["/"] fragmentRelPath) o);
+      host = (getString 0) + "-" + (getString 2);
+      dc = getString 1;
+      bin = ''
+        bin=$(nix build .#${dc}.${host}.system --no-link --print-out-paths)/sw/bin
+        export PATH=$bin:$PATH
+      '';
+    in (l.attrsets.mapAttrsToList (name: description: (mkCommand currentSystem {
+        inherit name description;
+        command =
+          bin
+          + "${
+            l.optionalString (l.elem name ["switch" "boot" "test" "dry-activate"]) "run0 --setenv=PATH=\"$PATH\" "
+          }nixos-rebuild ${name} --flake . -L --show-trace";
+      })) {
+        switch = "Activate & set as default boot.";
+        boot = "Set default boot, run old config.";
+        test = "Build & activate, no boot entry.";
+        build = "Build new config.";
+        dry-build = "Simulate build, show changes.";
+        dry-activate = "Simulate activation, show changes.";
+        edit = "Edit configuration with default editor.";
+        repl = "Opens the configuration in nix repl.";
+        build-vm = "Build script for NixOS virtual machine.";
+        build-vm-with-bootloader = "Build script with host-like bootloader.";
+        list-generations = "List system build generations.";
+      });
   };
 in
   nixosConfigurations
